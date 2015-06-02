9b7cca086cc024c9a4d70d38bda5bfadb3e9c57d
ascherbinin <ascherbinin@live.ru>
2015-06-02 12:43:33 +0700

    Initial Commit

---
+++ PhotoGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoGallery
+//
+//  Created by Андрей Щербинин on 02.06.15.
+//  Copyright (c) 2015 Андрей Щербинин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoGallery
+//
+//  Created by Андрей Щербинин on 02.06.15.
+//  Copyright (c) 2015 Андрей Щербинин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
